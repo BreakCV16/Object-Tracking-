85a8d6fe93583a9501a4e8be8a9288a77baeb7bf
jaewon Lee <jcircle67@gmail.com>
2020-10-30 18:37:33 +0900

    main structure
    main structure

---
+++ Main/Main/Main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	//도로 주행 영상
+	VideoCapture DrivingVideo("data/video.mp4");
+
+	Mat frame;
+
+	if (!DrivingVideo.isOpened())
+	{
+		cout << "Data can not open" << endl;
+		return -1;
+	}
+	VideoWriter writer;
+
+	namedWindow("TestSample");
+
+	while(1)
+	{
+		DrivingVideo >> frame;
+		if (frame.empty())
+			break;
+		imshow("TestSample", frame);
+		if (waitKey(10) > 0)
+			break;
+	}
+	return 0;
+}
+
