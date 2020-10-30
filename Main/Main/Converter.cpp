9b022b01c00d056f842d6802541027b563d85659
jaewon Lee <jcircle67@gmail.com>
2020-10-30 19:44:44 +0900

    add Converter function
    add Converter function not completed

---
+++ Main/Main/Converter.cpp
@@ -0,0 +1,21 @@
+#include "Converter.h"
+
+void RGBtoHSI(uchar r, uchar g, uchar b, double HSI[])
+{
+
+}
+
+void HSItoRGB(double h, double s, double i, unsigned char** rgb)
+{
+
+}
+
+void RGBtoHsv(uchar r, uchar g, uchar b, double Hsv[]) 
+{
+	double rn = (double)r / 255;
+}
+
+void HsvToRGB(double h, double s, double v, unsigned char** rgb)
+{
+
+}
--- Main/Main/Main.cpp
+++ Main/Main/Main.cpp
@@ -18,17 +18,27 @@ int main()
 	}
 	VideoWriter writer;
 
-	namedWindow("TestSample");
+	
 
 	while(1)
 	{
+		//원본 영상 읽어오기
 		DrivingVideo >> frame;
 		if (frame.empty())
 			break;
+
+		//미리 정해둔 흰색, 노란색 범위 내에 있는 부분만 차선 후보로 따로 저장하는거
+
+		//그레이 스케일 영상으로 변환하여 Edge 추출
+		//
 		imshow("TestSample", frame);
 		if (waitKey(10) > 0)
 			break;
 	}
 	return 0;
 }
-
+/*
+void color_detecting(Mat img_in, Mat& Converted_img)
+{
+	
+}*/
